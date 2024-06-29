2018235611ad8a2cfbfe13d1e9e456c2b27f06bd
Hardik Raval <hardikrawal3838@gmail.com>
2024-06-29 12:09:26 +0530

    responsive

--- src/QuoteBox.jsx
+++ src/QuoteBox.jsx
@@ -8,7 +8,7 @@ const QuoteBox = ({ quote, author, fetchQuote }) => {
       <button 
         id="new-quote" 
         onClick={fetchQuote} 
-        className="bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-600 transition duration-300 shadow-md mr-20"
+        className="bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-600 transition duration-300 shadow-md mr-20 md:mr-10"
       >
         New Quote
       </button>
@@ -17,7 +17,7 @@ const QuoteBox = ({ quote, author, fetchQuote }) => {
         href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`} 
         target="_blank" 
         rel="noopener noreferrer" 
-        className="inline-block text-teal-400 hover:underline transition duration-300 ml-20"
+        className="inline-block text-teal-400 hover:underline transition duration-300 ml-20 md:ml-10"
       >
         Tweet
       </a>
